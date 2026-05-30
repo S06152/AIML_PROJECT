6b61c9258379926aa1514d9efe87ea0c4c53d227
Sanjeev Kumar <sanjeev.ece.imps@gmail.com>
2026-05-30 15:59:00 +0530

    update

--- Project_9_Agentic_RAG/src/ui/streamlit_app.py
+++ Project_9_Agentic_RAG/src/ui/streamlit_app.py
@@ -135,8 +135,10 @@ class StreamlitApp:
             logging.info("Starting RAG ingestion pipeline.")
 
             # Step 1: Load PDFs
-            loader = PDFLoader(uploaded_file, user_controls)
-            documents = loader.load_documents()
+            loaders = [PDFLoader(file, user_controls) for file in uploaded_file]
+            documents = []
+            for loader in loaders:
+                documents.extend(loader.load_documents())
             logging.info(f"✅ PDF loaded: {len(documents)} pages extracted.")
 
             # Step 2: Chunk documents
